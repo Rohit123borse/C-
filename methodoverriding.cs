b1d0006d14a30a0d0b77cdc70a2f0ba5d7e05b8c
BORSE ROHIT LAXMAN <100746004+Rohit123borse@users.noreply.github.com>
2023-02-02 15:42:51 +0530

    Add files via upload

---
+++ methodoverriding.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace methodoverriding
+{
+    class demo
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Base class function");
+        }
+    }
+    class demo1:demo
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("derived class function");
+        }
+
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            demo1 d1 = new demo1();
+            d1.show();
+            Console.ReadLine();
+        }
+    }
+}
